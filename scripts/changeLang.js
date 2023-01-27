cd097228ff05a93aced105ac2da502868ba029b2
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-01-27 23:31:57 +0800

    KT Disclosure: 26 Jan 2023
    also implemented:
    - changes to header notification
    - change language JS script
    
    this disclosure reports about the update on the copyright infringement that KT sent to authorities regarding unauthorized reproduction of materials from DEAD OR ALIVE 6 for.. body pillow covers??!

---
+++ scripts/changeLang.js
@@ -0,0 +1,13 @@
+function changeToEN() {
+
+  document.getElementById("lang-en").style.display = "block";
+  document.getElementById("lang-ja").style.display = "none";
+
+}
+
+function changeToJA() {
+
+  document.getElementById("lang-en").style.display = "none";
+  document.getElementById("lang-ja").style.display = "block";
+
+}
