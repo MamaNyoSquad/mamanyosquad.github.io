dcce19e1d3429dcffa9d7e73550a2bca89b262d8
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-06-08 02:53:24 +0000

    Maintenance Details updated for Japanese Edition

--- scripts/maintenance.js
+++ scripts/maintenance.js
@@ -3,7 +3,7 @@ let maintenance = ["queued", "active", "finished"];
 
 //Japanese Edition
 
-const JPQueued = new Date('May 26, 2023 10:00:00 GMT+09:00').getTime();
+const JPQueued = new Date('June 8, 2023 12:00:00 GMT+09:00').getTime();
 
 const JPqueued = setInterval(function() {
   
@@ -21,7 +21,7 @@ const JPqueued = setInterval(function() {
 
 }, 1000);
 
-const JPActive = new Date('May 26, 2023 18:00:00 GMT+09:00').getTime();
+const JPActive = new Date('June 8, 2023 18:00:00 GMT+09:00').getTime();
 
 const JPactive = setInterval(function() {
   
