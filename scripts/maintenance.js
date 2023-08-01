4a5b694a180fcd7dd990fcd7820f40594ec23b3c
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-08-01 23:34:26 +0800

    Maintenance Details updated for Global Edition

--- scripts/maintenance.js
+++ scripts/maintenance.js
@@ -40,7 +40,7 @@ const JPactive = setInterval(function() {
 
 //Global Edition
 
-const GLQueued = new Date('July 20, 2023 02:30:00 GMT+00:00').getTime();
+const GLQueued = new Date('August 2, 2023 02:30:00 GMT+00:00').getTime();
 
 const GLqueued = setInterval(function() {
   
@@ -58,7 +58,7 @@ const GLqueued = setInterval(function() {
 
 }, 1000);
 
-const GLActive = new Date('July 20, 2023 06:30:00 GMT+00:00').getTime();
+const GLActive = new Date('August 2, 2023 06:30:00 GMT+00:00').getTime();
 
 const GLactive = setInterval(function() {
   
