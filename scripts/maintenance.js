3c940e9e308037056bab858e21a9560adbc56a04
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-11-17 18:31:19 +0800

    Maintenance Details updated for Japanese Edition

--- scripts/maintenance.js
+++ scripts/maintenance.js
@@ -6,8 +6,8 @@ const maintToday = new Date().getTime();
 
 function jMaint() {
 
-  const jpActive = new Date("November 9, 2023 10:00:00 GMT+09:00").getTime(),
-    jpEnd = new Date("November 9, 2023 17:00:00 GMT+09:00").getTime(),
+  const jpActive = new Date("November 18, 2023 10:00:00 GMT+09:00").getTime(),
+    jpEnd = new Date("November 18, 2023 18:00:00 GMT+09:00").getTime(),
     unixDetDist = jpEnd - jpActive,
     unixActDist = jpEnd - maintToday;
   
