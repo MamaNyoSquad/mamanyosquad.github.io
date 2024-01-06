eeed774a948a43252fbfa93dadf2f6b47f48a45d
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-01-06 14:03:25 +0800

    minor adjustments regarding some things

--- scripts/offlineSW.js
+++ scripts/offlineSW.js
@@ -1,9 +1,6 @@
 // This is the "Offline page" service worker
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-);
-
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
