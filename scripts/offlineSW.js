97034c5317d7666652edb9aaef9f79b7ccbdf3ee
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-01-16 12:08:34 +0800

    some minor changes to stuff in correlation to SINoALCE True Finale

--- scripts/offlineSW.js
+++ scripts/offlineSW.js
@@ -4,7 +4,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "../pwaoffline.html";
+const offlineFallbackPage = "pwaoffline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
