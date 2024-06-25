173bdb73a9a6dc97dd8843e4f70832ebc34ed823
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-06-25 20:15:38 +0800

    20240625
    in this update: all active pages that have the banner are updated to include any interactive URLs to them in anticipation of the 30th Overall Venus!!
    
    also moved some pages to `root` in observance to the new site structure (thanks to the nginx implementation which I initially worked on for `mamanyosquad-preview`)

--- scripts/events.js
+++ scripts/events.js
@@ -1,15 +1,23 @@
 // detect if site is WebApp
 
-const parseQuery = new URLSearchParams(window.location.search); // global value
+function specialEvent() { // special events page
 
-function specialEvent() {
-  const isWebApp = parseQuery.get("eventDismissed");
+  const isWebApp = new URLSearchParams(window.location.search).get("eventDismissed");
+
   if (isWebApp == "1") {
     console.log("event not triggered");
   } else {
     console.log("event triggered");
     // window.location.replace("events/tiananmen-anniversary");
   }
+
 }
 // code: https://sitepoint.com/get-url-parameters-with-javascript 
 // and https://github.com/VTresMarias/vtresmarias.github.io 
+
+function eventBanner() { // banner event
+  
+  var url = "wk131eoZGDk";
+  window.open("https://youtu.be/" + url);
+
+}
