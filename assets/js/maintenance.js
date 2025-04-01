aaa913f35d91b12b4ae6b6bf5b8b845581a59d62
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2025-04-01 10:29:02 +0800

    v20240518.20.12
    in this update:
    - fetch latest Maintenance upstream for Global

--- assets/js/maintenance.js
+++ assets/js/maintenance.js
@@ -56,7 +56,7 @@ function glMaint (sMt, sDy, sHh, sMm, eMt, eDy, eHh, eMm, tzz) {
   // below code are contained in <li important>, toggle the view with #impt-[locale]
   let toggle = ["none", "list-item"];
 
-  document.getElementById("impt-en_gl").style.display = toggle[1];
+  document.getElementById("impt-en_gl").style.display = toggle[0];
 
   // document.getElementById("impt-en_gl").innerHTML = "task here]";
 
