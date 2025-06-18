5d3881115cdcdafe2d35fc88691c57a12fe1f8cc
💚🍃Aga-chuu💗🌸 <103877195+thebelovedmoon@users.noreply.github.com>
2025-06-18 16:12:43 +0000

    v20240518.20.25 changelog: - updates to Maintenance Routines (Japanese and Global)

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -11,7 +11,7 @@ function init() {
 }
 
 function showVersion() {
-  document.getElementById("siteVersion").innerHTML = "v20240518.20.24";
+  document.getElementById("siteVersion").innerHTML = "v20240518.20.25";
 }
 
 // js clock code adapted from w3schools.com/js/tryit.asp?filename=tryjs_timing_clock
