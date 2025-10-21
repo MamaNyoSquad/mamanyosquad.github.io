704db3baa523a73d62e41302d34b7fca09dff676
💚🍃Aga-chuu💗🌸 <103877195+thebelovedmoon@users.noreply.github.com>
2025-10-21 12:47:27 +0000

    v20240518.22.21 changelog: - updated Maintenance Routine for Global Edition - minor visual fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -46,7 +46,7 @@ function supplementalBtns() {
   for (let b = 0; b < suplRows.length; b++) {
     suplRows[b].insertAdjacentHTML("beforeend", `
       <!-- <span class="material-symbols-rounded xvvSurvey" title="#MamaNyoSquad&ensp;&bull; LIVE presents DOAXVV @ TGS 2025!" onclick="window.open('https://youtu.be/mNsy-usRsCc')">play_circle</span> -->
-      <span class="material-symbols-sharp xvvSurvey" title="What happens to DOAXVV and PRISM the moment Windows 10 goes out of support? Watch this VOD to learn more!" onclick="ytVodDialog('en9ioABcos4', 'width: 950px; height: auto; aspect-ratio: 16 / 9;')">border_all</span>
+      <!-- <span class="material-symbols-sharp xvvSurvey" title="What happens to DOAXVV and PRISM the moment Windows 10 goes out of support? Watch this VOD to learn more!" onclick="ytVodDialog('en9ioABcos4', 'width: 950px; height: auto; aspect-ratio: 16 / 9;')">border_all</span> -->
       <!-- <span class="material-symbols-rounded xvvSurvey" title="See our election coverage" onclick="window.open('/MamaNyoVotesPH')">how_to_vote</span> -->
       <!-- <span class="material-symbols-rounded xvvSurvey" title="Explore the Venus Vacation Collection" onclick="window.open('/VenusVacationCollection')">beach_access</span> -->
       <span class="material-symbols-rounded xvvSurvey" title="Listen to PRISM's theme &quot;Stolen&quot;!" onclick="xvvDialog('music', 'fgXv8wMl3h0')">music_note</span>
