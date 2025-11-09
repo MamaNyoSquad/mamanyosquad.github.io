03f55e1dad7aa05bcdef86a38ebe45e2f447e245
💚🍃Aga-chuu💗🌸 <103877195+thebelovedmoon@users.noreply.github.com>
2025-11-09 09:44:37 +0000

    v20240518.22.22 changelog: - updated Maintenance Routines for Japanese and Global Editions

--- assets/js/maintenance.js
+++ assets/js/maintenance.js
@@ -30,11 +30,11 @@ function jpMaint(sMt, sDy, sHh, sMm, eMt, eDy, eHh, eMm, tzz) {
   // as there's no vanillajs-equivalent of general selector yet
   // and vanillajs doesn't permit outputting data to duplicate
   // ids in its associated html.
-  document.getElementById("impt-ja_jp").setAttribute("style", `display: ${toggle[0]};`);
-  document.getElementById("impt-en_jp").setAttribute("style", `display: ${toggle[0]};`);
+  document.getElementById("impt-ja_jp").setAttribute("style", `display: ${toggle[1]};`);
+  document.getElementById("impt-en_jp").setAttribute("style", `display: ${toggle[1]};`);
 
-  document.getElementById("impt-ja_jp").innerHTML = "レスレリアーナのアトリエ × DOAXVV";
-  document.getElementById("impt-en_jp").innerHTML = "Atelier Resleriana × DOAXVV";
+  document.getElementById("impt-ja_jp").innerHTML = "8nfinityのDOAXVV";
+  document.getElementById("impt-en_jp").innerHTML = "DOAXVV @ 8nfinity";
 
   // True Colors (comment to activate / deactivate)
   // document.getElementById("impt-ja_jp").innerHTML = "True Colors～モニカ～";
@@ -56,12 +56,12 @@ function glMaint (sMt, sDy, sHh, sMm, eMt, eDy, eHh, eMm, tzz) {
   // below code are contained in <li important>, toggle the view with #impt-[locale]
   let toggle = ["none", "list-item"];
 
-  document.getElementById("impt-en_gl").setAttribute("style", `display: ${toggle[1]};`);
+  document.getElementById("impt-en_gl").setAttribute("style", `display: ${toggle[0]};`);
 
-  // document.getElementById("impt-en_gl").innerHTML = "DOAXVV Global @ 6: 6th Prismic Revelation";
+  document.getElementById("impt-en_gl").innerHTML = "DOAXVV Global @ 6: 6th Prismic Revelation";
 
   // True Colors (comment to activate / deactivate)
-  document.getElementById("impt-en_gl").innerHTML = "DOAXVV ends support for Windows 10";
+  // document.getElementById("impt-en_gl").innerHTML = "True Colors ～Kokkoro～";
 
 }
 
