cc0e554c253c852aae1c87453b8aa4d6592f9d08
💚🍃Aga-chuu💗🌸 <103877195+thebelovedmoon@users.noreply.github.com>
2026-08-23 01:32:51 +0000

    v20230518.23.16 changelogs: - some changes made to `onclick` to encapsulate in `{}` - somehow overlooked the description for holy week press release for years.. FIXED - correcting misgendering for G.Mgr (now she goes with the correct pronouns!!)
    _I just noticed that `git lfs` BREAKS txtfiles.. buuuut we'll see_

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,13 +45,13 @@ function supplementalBtns() {
   let suplRows = document.querySelectorAll(".btnOther");
   for (let b = 0; b < suplRows.length; b++) {
     suplRows[b].insertAdjacentHTML("beforeend", `
-      <!-- <span class="material-symbols-rounded xvvSurvey" title="#MamaNyoSquad&ensp;&bull; LIVE presents DOAXVV @ TGS 2025!" onclick="window.open('https://youtu.be/mNsy-usRsCc')">play_circle</span> -->
-      <!-- <span class="material-symbols-sharp xvvSurvey" title="What happens to DOAXVV and PRISM the moment Windows 10 goes out of support? Watch this VOD to learn more!" onclick="ytVodDialog('en9ioABcos4', 'width: 950px; height: auto; aspect-ratio: 16 / 9;')">border_all</span> -->
-      <!-- <span class="material-symbols-rounded xvvSurvey" title="See our election coverage" onclick="window.open('/MamaNyoVotesPH')">how_to_vote</span> -->
-      <!-- <span class="material-symbols-rounded xvvSurvey" title="Explore the Venus Vacation Collection" onclick="window.open('/VenusVacationCollection')">beach_access</span> -->
-      <span class="material-symbols-rounded xvvSurvey" title="Listen to PRISM's theme &quot;Stolen&quot;!" onclick="xvvDialog('music', 'fgXv8wMl3h0')">music_note</span>
-      <!-- <span class="material-symbols-rounded xvvNotify" title="More colors, more fun -- now available on #MamaNyoSquad v20240518.23.0!" onclick="xvvDialog('newver')">release_alert</span> -->
-      <!-- <span class="material-symbols-rounded xvvEvent" title="Check out the newest event from DOAXVV!" onclick="xvvDialog('event', '${encodeURIComponent(`"jp", "info/49788"`)}')">celebration</span> -->
+      <!-- <span class="material-symbols-rounded xvvSurvey" title="#MamaNyoSquad&ensp;&bull; LIVE presents DOAXVV @ TGS 2025!" onclick="{ window.open('https://youtu.be/mNsy-usRsCc'); }">play_circle</span> -->
+      <!-- <span class="material-symbols-sharp xvvSurvey" title="What happens to DOAXVV and PRISM the moment Windows 10 goes out of support? Watch this VOD to learn more!" onclick="ytVodDialog('en9ioABcos4', 'width: 950px; height: auto; aspect-ratio: 16 / 9;'); }">border_all</span> -->
+      <!-- <span class="material-symbols-rounded xvvSurvey" title="See our election coverage" onclick="{ window.open('/MamaNyoVotesPH'); }">how_to_vote</span> -->
+      <!-- <span class="material-symbols-rounded xvvSurvey" title="Explore the Venus Vacation Collection" onclick="{ window.open('/VenusVacationCollection'); }">beach_access</span> -->
+      <span class="material-symbols-rounded xvvSurvey" title="Listen to PRISM's theme &quot;Stolen&quot;!" onclick="{ xvvDialog('music', 'fgXv8wMl3h0'); }">music_note</span>
+      <!-- <span class="material-symbols-rounded xvvNotify" title="More colors, more fun -- now available on #MamaNyoSquad v20240518.23.0!" onclick="{ xvvDialog('newver'); }">release_alert</span> -->
+      <!-- <span class="material-symbols-rounded xvvEvent" title="Check out the newest event from DOAXVV!" onclick="{ xvvDialog('event', '${encodeURIComponent(`"jp", "info/49788"`)}'); }">celebration</span> -->
     `);
   }
 }
